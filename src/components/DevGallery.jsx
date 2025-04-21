3bdfe0a7fe8beedc4885b5cc7b75a12822428f89
Katie Baeten <katherinebaeten@gmail.com>
2025-04-21 14:36:02 -0500

    Added dev gal, updated layout and modal UI

--- src/components/DevGallery.jsx
+++ src/components/DevGallery.jsx
@@ -1,25 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import ArtCard from "./ArtCard";
-
+import DevDetailModal from "./DevDetailModal";
+import movingEyesImage from "../assets/dev/SofonisbaAnguissola.png";
+import pacManImage from "../assets/dev/pacmanFactory.png";
+import mitBusImage from "../assets/dev/realTimeBus.png";
 export default function DevGallery() {
+	const [activeProject, setActiveProject] = useState(null);
+
 	const devProjects = [
 		{
 			id: "dev1",
-			title: "Portfolio Site",
-			image: "path/to/image.jpg",
-			description: "My personal dev portfolio site using React and Tailwind.",
+			title: "Moving Eyes",
+			image: movingEyesImage,
+			description: "SAMPLE INFORMATION",
+			githubURL: "",
+			tooltip: "scooby doo style css tricks"
 		},
-		// More dev projects...
+		{
+			id: "dev2",
+			title: "PacMan",
+			image: pacManImage,
+			description: "",
+			githubURL: "",
+			tooltip: "pacman factory",
+		},
+		{
+			id: "dev3",
+			title: "MIT Bus Routes",
+			image: mitBusImage,
+			description:
+				"This project made use of mapbox-gl api and public data from MIT to track busses between the Harvard and MIT campuses. Using a starter code outline I hard coded coordinate updates. I extended the code to update the busses' positions in real-time.",
+			githubURL: "",
+			tooltip: "mapbox-gl api and public data from MIT to track busses",
+		},
 	];
+
+	const handleCardClick = (proj) => {
+		setActiveProject(proj.id === activeProject?.id ? null : proj);
+	};
 
 	return (
 		<div>
 			<h2>Engineering Projects</h2>
 			<div className='gallery-flex'>
 				{devProjects.map((proj) => (
-					<ArtCard key={proj.id} artwork={proj} onClick={() => {}} />
+					<ArtCard
+						key={proj.id}
+						artwork={proj}
+						onClick={() => handleCardClick(proj)}
+					/>
 				))}
 			</div>
+
+			{activeProject && (
+				<DevDetailModal
+					project={activeProject}
+					onClose={() => setActiveProject(null)}
+				/>
+			)}
 		</div>
 	);
 }
